4fddd6ffd64d58675b25fba6085fa92cbfb1d2e7
Hackbright Student <info@hackbrightacademy.com>
2016-05-06 12:25:07 -0700

    Completed testing fixes

---
+++ model.py
@@ -0,0 +1,35 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.Model):
+    """Board game."""
+
+    __tablename__ = "games"
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    description = db.Column(db.String(100))
+
+
+def connect_to_db(app, db_uri="postgresql:///games"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    db.app = app
+    db.init_app(app)
+
+
+def example_data():
+    """Create example data for the test database."""
+
+    Game.query.delete()
+    
+    game1 = Game(name="Amazing Labyrinth", description="move around the shifting paths of the labyrinth in a race to collect various treasures")   
+
+    db.session.add(game1)
+    db.session.commit()
+
+if __name__ == '__main__':
+    from server import app
+
+    connect_to_db(app)
+    print "Connected to DB."
